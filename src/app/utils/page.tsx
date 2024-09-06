967f7d9df142e1580fc0a528b6844630f720b023
Sam <sam@justcallmesam.com>
2024-09-05 21:32:01 -0500

    purge

--- src/app/utils/page.tsx
+++
@@ -1,7 +0,0 @@
-export default function Utils() {
-  return (
-    <main className="relative flex flex-col gap-10 py-10 justify-center items-center w-full h-auto">
-      <span>Check back later...</span>
-    </main>
-  );
-}
